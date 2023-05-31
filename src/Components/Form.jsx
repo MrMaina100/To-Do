7c9ad61c212a64ab0b6a6f5d8a57e36a2718f104
Derrick Maina <mburumaina648@gmail.com>
2023-05-31 16:03:55 +0300

    handled form input

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,13 +1,47 @@
-function Form() {
-  return (
-    <div>
-      <form action="">
-         <input type="text" placeholder="write a task" className="border-b w-full focus:outline-none placeholder:italic placeholder:font-serif"/>
+import { useState } from "react";
+function Form() { 
+  
+  const [textInput, setTextInput] = useState({
+    input:''
+    
+  }); 
 
-      </form>
-      <button className="bg-black text-white px-8 py-2 rounded-md mt-2">
+  
+
+  const handleSubmit = (e)=>{
+    e.preventDefault()  
+  
+    console.log(textInput);   
+
+  }
+
+
+  const handleChange = (e)=>{
+    const {name, value} = e.target;
+    setTextInput({
+      ...textInput,
+      [name]: value
+    });      
+
+  }
+
+  
+
+  return (
+    
+    <div>      
+     
+      <form onSubmit={handleSubmit}>
+         <input type="text" placeholder="write a task" className="border-b w-full focus:outline-none placeholder:italic placeholder:font-serif" 
+         onChange={handleChange} value={textInput.input} name="input" />  
+
+         
+           <button type="Submit" className="bg-black text-white px-8 py-2 rounded-md mt-2" >
          Add
       </button>
+
+      </form>
+    
     </div>
   )
 }

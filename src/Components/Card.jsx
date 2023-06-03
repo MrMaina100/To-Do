0e2d42c3f665589f16b0ac3bfc6518b679bddfa9
Derrick Maina <mburumaina648@gmail.com>
2023-06-03 13:43:08 +0300

    changed styles for the cards

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -25,7 +25,7 @@ function Card() {
 
 
   return (
-    <div className="p-10 mt-10 bg-white flex flex-col space-y-4 md:p-16  w-[19rem] md:mt-10 md:w-[34rem]  md:space-y-5 rounded-xl">
+    <div className="p-10 mt-10 bg-white flex flex-col space-y-4 md:p-16  w-[19rem] md:mt-10 md:w-[34rem]  md:space-y-2 rounded-xl">
 
       <Header/>
       <Form  setSubItem={setSubItem} />      
